bbfdf216b4d5d3b30336243bfe5943884f606783
Jozef <knaperek@users.noreply.github.com>
2016-06-23 23:44:32 +0200

    Add option to specify default included resources (#250)
    * Add option to specify default included resources
    
    Let's provide an extra JSONAPIMeta configuration option that specifies
    resources to be listed in the 'included' section of the response.
    These resources shall be included even if they're not explicitly mentioned
    in the 'include' request parameter.
    
    * Re-trigger Travis Build script
    
    The previous build failure was caused by some
    random environment issue.
    
    * Ignore default 'included' resources if specified in request
    
    Conform to the specs and ensure that no other resources are included
    in the response other than those explicitely requested inside the
    'include' parameter in request.
    
    * Add tests for default included_resources feature

--- rest_framework_json_api/utils.py
+++ rest_framework_json_api/utils.py
@@ -242,6 +242,13 @@ def get_resource_type_from_serializer(serializer):
     raise AttributeError()
 
 
+def get_default_included_resources_from_serializer(serializer):
+    try:
+        return list(serializer.JSONAPIMeta.included_resources)
+    except AttributeError:
+        return []
+
+
 def get_included_serializers(serializer):
     included_serializers = copy.copy(getattr(serializer, 'included_serializers', dict()))
 
